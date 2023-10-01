b5a3ecd91140f4be2bebebd6031e205e92c7c08c
dropkun <junya111226@gmail.com>
2023-10-02 07:20:40 +0900

    cloudflare

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Container from '@/components/container';
 import Hero from '../components/hero';
-export const runtime = 'edge';
 
 export default function Home({}) {
   return (
@@ -9,3 +8,4 @@ export default function Home({}) {
     </Container>
   );
 }
+
